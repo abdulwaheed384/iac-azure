227949181e841c585a1c23e5729439123ed55ace
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-04-27 16:40:03 +0100

    update storage

--- main.tf
+++ main.tf
@@ -12,5 +12,9 @@ resource "azurerm_storage_account" "storage" {
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
+
+  enable_https_traffic_only = true
+  allow_blob_public_access  = false
+
 }
