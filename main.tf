73b32d8d8c92b7c39faf98c552e9ea74068600fe
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-04-27 16:03:01 +0100

    storage fix

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "storage-aw-demo-78645"
+  name                     = "storage7864500"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
