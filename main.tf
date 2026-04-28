564108aeebc01d199d036789812ade4c4bf1398f
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-04-28 11:33:15 +0100

    vnet

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
 provider "azurerm" {
   features {}
 }
-
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-demo-ai-project"
-  location = "UK South"
-}
-
-resource "azurerm_storage_account" "storage" {
-  name                     = "storage07864500"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  enable_https_traffic_only = true
-  
-
-}
---
+++ network.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "network_rg" {
+  name     = "rg-network-demo-ai"
+  location = "UK South"
+
+  tags = {
+    environment = "dev"
+    project     = "ai-iac"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-demo-ai-project"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.network_rg.location
+  resource_group_name = azurerm_resource_group.network_rg.name
+
+  tags = {
+    environment = "dev"
+    project     = "ai-iac"
+  }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-demo"
+  resource_group_name  = azurerm_resource_group.network_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
