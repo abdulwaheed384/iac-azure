b8c0fd5090babc1b272364729d99f8d0f65d872c
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-04-29 12:36:03 +0100

    nsg

---
+++ nsg.tf
@@ -0,0 +1,34 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "iac-rg"
+  location = "UK South"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "iac-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
