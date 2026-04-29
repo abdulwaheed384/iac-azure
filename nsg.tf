cee303f7e8c45b3be41e46a3affb2910714e4c7a
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-04-29 17:19:48 +0100

    ai-3

--- nsg.tf
+++ nsg.tf
@@ -22,7 +22,7 @@ resource "azurerm_network_security_group" "nsg" {
 
   security_rule {
     name                       = "Allow-HTTP"
-    priority                   = 110
+    priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
