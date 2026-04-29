7398f2a932d75afb8775ba73441e2ef08e4ea239
Abdul Waheed <abdul_waheed384@hotmail.com>
2026-04-29 17:25:04 +0100

    ai-4

--- network.tf
+++ network.tf
@@ -14,7 +14,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.network_rg.location
   resource_group_name = azurerm_resource_group.network_rg.name
 
-  dns_servers = ["80.80.80.80", "40.2.2.2"]
+  dns_servers = ["80.80.80.80", "40.20.20.20"]
 
 
   tags = {
